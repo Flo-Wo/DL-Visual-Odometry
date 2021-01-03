d54b300d663caf3cb7bd8e6f2d73c831a89b7caf
SenorDonPaco <znarf12345@outlook.com>
2021-01-03 19:58:13 +0100

    Data Loader

--- project/src/data_loader.py
+++ project/src/data_loader.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 29 10:36:35 2020
+Created on Sun Jan 03.01.2021
 
-@author: florianwolf
+@author: franzherbst
 """
 
 import torch
